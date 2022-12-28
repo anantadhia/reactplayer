c7c9c239385bf30c750ed232e8d32ad1ada8354c
anantadhia <102227869+anantadhia@users.noreply.github.com>
2022-12-28 18:42:33 +0700

    songs son!

--- src/data.js
+++ src/data.js
@@ -36,10 +36,20 @@ function chillHop() {
       name: "Zutto Ishosha",
       cover:
         "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-      album: "Swørn",
+      album: "Single",
       audio:
         "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/Zutto%20Ishosha.mp3",
       color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Someday",
+       cover: "/Rideontime.jpg",
+      album: "Ride  on Time",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/Someday.mp3",
+      color: ["#CD207D", "#c74252"],
       id: uuidv4(),
       active: false,
     },
@@ -56,7 +66,7 @@ function chillHop() {
     {
       name: "Ride  on Time (Single Ver.)",
       cover: "/Rideontime.jpg",
-      album: "Ride  on Timee",
+      album: "Ride  on Time",
       audio:
         "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/10.-RIDE-ON-TIME-_Single_.mp3",
       color: ["#EF8EA9", "#ab417f"],
@@ -70,6 +80,16 @@ function chillHop() {
       audio:
         "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/Tatsuro%20Yamashita%20%20Fragile.mp3.webm",
       color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Oyasumi",
+       cover: "/Rideontime.jpg",
+      album: "Ride  on Time",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/Oyasumi.mp3",
+      color: ["#CD207D", "#c74252"],
       id: uuidv4(),
       active: false,
     },
@@ -90,6 +110,16 @@ function chillHop() {
       audio:
         "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/04.%20Futari.mp3",
       color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Kumo no yukue ni",
+       cover: "/Rideontime.jpg",
+      album: "Ride  on Time",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/Kumo%20no%20yukue%20ni.mp3",
+      color: ["#CD207D", "#c74252"],
       id: uuidv4(),
       active: false,
     },
@@ -232,7 +262,167 @@ function chillHop() {
       id: uuidv4(),
       active: false,
     },
-
+{
+      name: " My Memories Of You",
+      cover: "/OTSC2.jpg",
+      album: "On The Street Corner 2",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/05.%20My%20Memories%20Of%20You.mp3",
+      color: ["#CD607D", "#c74253"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Human",
+     cover: "/Artisan.jpg",
+      album: "Artisan",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/06%20----Human.mp3",
+      color: ["#CD607D", "#c74253"],
+      id: uuidv4(),
+      active: false,
+    },
+      {
+      name: "Your Eyes",
+       cover: "/ForY.jpg",
+      album: "For You",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/08.%20Your%20Eyes.mp3",
+      color: ["#CD607D", "#c74253"],
+      id: uuidv4(),
+      active: false,
+    },
+     {
+      name: "Chapel Of Dreams",
+      cover: "/OTSC2.jpg",
+      album: "On The Street Corner 2",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/06.%20Chapel%20Of%20Dreams.mp3",
+      color: ["#CD207D", "#c74253"],
+      id: uuidv4(),
+      active: false,
+    }, 
+    {
+      name: "City Way",
+      cover: "/CircusTown.jpg",
+      album: "Circus Town",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/06.%20City%20Way.mp3",
+      color: ["#CD207D", "#c74253"],
+      id: uuidv4(),
+      active: false,
+    },
+     {
+      name: "Hey Reporter",
+      cover: "/ForY.jpg",
+      album: "For You",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/07.%20Hey%20Reporter!.mp3",
+      color: ["#CD207D", "#c74253"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Splendor",
+      cover: "/Artisan.jpg",
+      album: "Artisan",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/07%20Splendor.mp3",
+      color: ["#CD207D", "#c74253"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Mayoikonda machi to",
+      cover: "/CircusTown.jpg",
+      album: "Circus Town",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/07.%20Mayoikonda%20machi%20to.mp3",
+      color: ["#CD207D", "#c74253"],
+      id: uuidv4(),
+      active: false,
+    },
+     {
+      name: "You Make Me Feel Brand New",
+       cover: "/OTSC2.jpg",
+      album: "On The Street Corner 2",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/07.%20You%20Make%20Me%20Feel%20Brand%20New.mp3",
+      color: ["#CD207D", "#c74253"],
+      id: uuidv4(),
+      active: false,
+    },
+     {
+      name: "Mighty Smile",
+       cover: "/Artisan.jpg",
+      album: "Artisan",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/08%20Mighty%20Smile%20(------).mp3",
+      color: ["#CD207D", "#c74253"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "I Only Have Eyes For You",
+      cover: "/OTSC2.jpg",
+      album: "On The Street Corner 2",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/08.%20I%20Only%20Have%20Eyes%20For%20You.mp3",
+      color: ["#CD207D", "#c74253"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "夏の陽 (Natsu No Hi)",
+      cover: "/CircusTown.jpg",
+      album: "Circus Town",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/08.%20Natsu%20no%20hi.mp3",
+      color: ["#CD207D", "#c74253"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Funky Flushin_ (1982年 Version)",
+      cover: "/Moon.jpg",
+      album: "Moonglow",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/13.%20Funky%20Flushin_%20(1982%E5%B9%B4%20Version).mp3",
+      color: ["#CD207D", "#c74252"],
+      id: uuidv4(),
+      active: false,
+    },
+     {
+      name: "Queen of Hype, Blues",
+      cover: "/Artisan.jpg",
+      album: "Artisan",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/09%20-Queen%20of%20Hype-%20Blues.mp3",
+      color: ["#CD207D", "#c74252"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Silent Night",
+      cover: "/melo.jpg",
+      album: "Melodies",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/09.%20Silent%20Night.mp3",
+      color: ["#CD207D", "#c74252"],
+      id: uuidv4(),
+      active: false,
+    },
+     {
+      name: "Ice Manicure",
+      cover: "/Cozy.jpg",
+      album: "Cozy",
+      audio:
+        "https://github.com/anantadhia/reactplayer/raw/production/src/Audio/1.%20Ice%20Manicure.mp3",
+      color: ["#CD207D", "#c74252"],
+      id: uuidv4(),
+      active: false,
+    },
+    
     //ADD MORE HERE
   ];
 }
